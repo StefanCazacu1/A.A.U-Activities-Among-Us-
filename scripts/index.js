aebc12c0ce580f7eb82badeb2e68ffd6fb87e11b
0Empty0 <victor.gargat28@gmail.com>
2023-11-11 13:35:19 +0100

    add image preview

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,17 @@
 console.log('HELLo, World!')
+
+const postForm = document.getElementById('postForm')
+const thumbnail = document.getElementById('thumbnail')
+
+postForm.addEventListener('change', e => {
+	if (e.target.id === 'image') {
+		thumbnail.src = e.target.value
+		const reader = new FileReader()
+
+		reader.onload = () => {
+			thumbnail.src = reader.result
+		}
+		reader.readAsDataURL(e.target.files[0])
+	}
+	console.log(e.target)
+})
